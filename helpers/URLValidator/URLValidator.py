16449904d39aa001f1566b9d206d22c298695bdd
Jackson Barreto <jackson.barreto@gmail.com>
2022-12-29 13:40:52 +0000

    Ensured that only public and valid IP addresses are accepted

--- helpers/URLValidator/URLValidator.py
+++ helpers/URLValidator/URLValidator.py
@@ -5,8 +5,12 @@ class URLValidator:
     def __init__(self, url):
         url_validation_regex = re.compile(
             r'^(?:(http)s?://)?'  # http:// or https:// (optional)
+            r'(?!(?:10|172\.(?:1[6-9]|2[0-9]|3[01])|192\.168)\.)'  # negative lookahead for private IP address ranges
             r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', re.IGNORECASE)
+            r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))'  # or IP with range check for each octet
+            r'(?:/[^\s]*)?'  # optional slash and query parameters
+            r'(?:#\S*)?'  # optional fragment identifier
+            r'$', re.IGNORECASE)
         if re.match(url_validation_regex, url):
             self.url = url
             self.domain = re.findall(r'^(?:https?://)?(?:[^@/\n]+@)?([^:/?\n]+)', url)[0]
--- helpers/URLValidator/test_URLValidator.py
+++ helpers/URLValidator/test_URLValidator.py
@@ -46,6 +46,42 @@ class URLValidatorTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.validator = URLValidator('www./meet/20842')
 
+    def test_invalid_private_ip_address(self):
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('http://10.0.0.1')
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('http://172.16.0.1')
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('http://192.168.0.1')
+
+    def test_valid_public_ip_address(self):
+        self.validator = URLValidator('http://8.8.8.8')
+        self.assertEqual(self.validator.get_url(), '8.8.8.8')
+        self.validator = URLValidator('http://64.233.160.0')
+        self.assertEqual(self.validator.get_url(), '64.233.160.0')
+        self.validator = URLValidator('http://23.253.163.0')
+        self.assertEqual(self.validator.get_url(), '23.253.163.0')
+
+    def test_invalid_ip_address_less_than_4_octets(self):
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('http://1.2.3')
+
+    def test_invalid_ip_address_value_above_255(self):
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('http://256.2.3.4')
+
+    def test_valid_public_ip_address_without_schema(self):
+        self.validator = URLValidator('8.8.8.8')
+        self.assertEqual(self.validator.get_url(), '8.8.8.8')
+
+    def test_invalid_private_ip_address_without_schema(self):
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('10.0.0.1')
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('172.16.0.1')
+        with self.assertRaises(ValueError):
+            self.validator = URLValidator('192.168.0.1')
+
 
 if __name__ == '__main__':
     unittest.main()
